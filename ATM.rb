e1d883b48983c7ad25dcbdd6cdbf6866051eceac
JediMarty <pulstar_martin@abv.bg>
2023-06-19 18:58:44 +0300

    begin

--- ATM.rb
+++ ATM.rb
@@ -1,1 +1,20 @@
+card = false
+
+while card==false
+    
+    puts "\t\tATM"
+    puts "\tPlease insert your card!"
+    insert = gets.chomp().downcase()
+    if insert == "card"
+    card = true
+    end
+
+end
+
+=begin
+puts "\t\tPlease choose service"
+puts "1: Withdrawal"
+puts "2: Deposit"
+puts "3: Balance Inquiry"
+=end
 
