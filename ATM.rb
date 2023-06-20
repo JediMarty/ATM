012d941cedc3d7423061900946f72c8dbcd630fc
JediMarty <pulstar_martin@abv.bg>
2023-06-20 11:41:02 +0300

    first update

--- ATM.rb
+++ ATM.rb
@@ -1,3 +1,16 @@
+
+class Person_card
+
+    attr_accessor :name, :money
+    
+    def initialize(name,money)
+        @name = name
+        @money = money
+    end
+
+end
+
+person1 = Person_card.new("Andrew",1200)
 card = false
 
 while card==false
@@ -11,10 +24,37 @@ while card==false
 
 end
 
-=begin
+def Withdrawal
+
+
+
+end
+
+def Deposit
+
+end
+
+def Balance_Inquiry(person1)
+    puts ("Your balance is " + person1.money.to_s + " leva")
+end
+
+
 puts "\t\tPlease choose service"
 puts "1: Withdrawal"
 puts "2: Deposit"
 puts "3: Balance Inquiry"
-=end
+choice = gets.chomp()
+
+case choice
+
+when "1"
+    
+ 
+when "2"
+
+when "3"
+    Balance_Inquiry(person1)
+end
+
+
 
